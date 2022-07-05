2c8a5782b23acfeae6662cd5c3a722d982468fe4
paginesher <paginesher@gmail.com>
2022-07-05 15:20:54 -0400

    push

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+//connect to database 
+mongoose.connect('mongodb+srv://hyeb96:Sm17emina@cluster0.yyfegbj.mongodb.net/hopehack', { useNewUrlParser: true }, { useUnifiedTopology: true })
+
+//create schema 
+const contactSchema = {
+    name: String,
+    email: String,
+    message: String
+}
+
+//create model
+const contact = mongoose.model("contact", contactSchema)
+
+app.post("/contact", (req, res) => {
+    let newContact = new contact({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message
+    });
+    newContact.save();
+    res.redirect('/');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+// for ejs files 
+app.set("view engine", "ejs")
+
+//route
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname + '/contact.html'));
+})
+
+//importing users routers and set it to starts with 'users'
+// const userRouter = require('./routes/users')
+// app.use('/users', userRouter)
+
+//create a server 
+app.listen(3000)
